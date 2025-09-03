51bf4a0b3c5ae3375c039abfedabc1b0658fbc37
chaichan05 <chaichan.k@ku.th>
2025-09-04 00:39:25 +0700

    first commit

---
+++ project/lib/client.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const ClientPage(),
+    );
+  }
+}
+
+class ClientPage extends StatelessWidget {
+  const ClientPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Image(
+          image: const AssetImage('assets/bbq.png'),
+          alignment: Alignment.centerLeft,
+        ),
+        toolbarHeight: 70,
+        centerTitle: true,
+        title: ConstrainedBox(
+          constraints: BoxConstraints(),
+          child: Text(
+            'IT BBQ',
+            style: GoogleFonts.playfairDisplay(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+            ),
+            textAlign: TextAlign.right,
+          ),
+        ),
+        backgroundColor: const Color(0xFFFA6C6B),
+      ),
+
+      body: const Center(
+        child: Text(
+          'ลูกค้า',
+          style: 
+          TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        
+      ),
+      backgroundColor: const Color(0xFFF6FBFE),
+    );
+  }
+}
